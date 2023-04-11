3ddc06bba4bad699d25af1af0a444517aae58a86
juraj.rudy <jrudy134@gmail.com>
2023-04-11 22:06:13 +0200

    login

--- AS-FrontEnd/App.js
+++ AS-FrontEnd/App.js
@@ -1,19 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './components/login';
+import Registration from './components/registration';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Registration" component={Registration} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Login/>
+    // </View>
   );
 }
-
 const styles = StyleSheet.create({
+  input: {
+    borderWidth: '3',
+    borderRadius: '5',
+    borderColor: '#000',
+    margin: 15,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#393e41',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ AS-FrontEnd/components/registration.js
@@ -0,0 +1,13 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { CognitoUserPool, CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
+
+const Registration = () =>{
+
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+export default Registration;
